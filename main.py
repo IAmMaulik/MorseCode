d1f52b73e6d1db606a30335d1bf5d50ae7beecfb
Maulik Shah <maulik.shah715@gmail.com>
2021-02-22 12:20:21 +0530

    Added a funtion to convert the text back into morse code

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from data import alphabet_to_morse, morse_to_alphabet
 
-def encode(string):
-    output_string = []
-    for i in range(len(string)):
-        output_string.append(alphabet_to_morse[string[i].upper()])
+def convert_to_arr(input_string):
+    # logic to convert the morse code string into a list 😶
+    pass    
 
-        if (i+1) == len(string):
+def encode(input_string):
+    # funtion to convert the text into morse code COMPLETE 🔥🔥🔥
+    output_arr = []
+    for i in range(len(input_string)):
+        output_arr.append(alphabet_to_morse[input_string[i].upper()])
+        if (i+1) >= len(input_string):
             break
-        elif string[i+1] == " ":
-            output_string.append("  ")
+        elif input_string[i+1] == " ":
+            output_arr.append("  ")
         else:
-            output_string.append(" ")
+            output_arr.append(" ")
+    return ''.join(output_arr)
+
+def decode(input_string):
+    # funtion to convert the morse code back into text COMPLETE 🔥🔥🔥
+    output_arr = []
+    li = convert_to_arr(input_string)
+    for i in range(len(li)):
+        if li[i] == " ":
+            output_arr.append(" ")
+        else:
+            output_arr.append(morse_to_alphabet[li[i]])
     
-    return ''.join(output_string)
+    return ''.join(li)
+
 
 print(encode(input("Enter the text you want to encode: ")))
